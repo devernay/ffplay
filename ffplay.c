a8fc1262a143511df274d8fbde7cec1db61dd47a
Frédéric Devernay <frederic.devernay@m4x.org>
2016-03-21 13:33:47 +0100

    audio_thread(): move variables to loop body

--- ffplay.c
+++ ffplay.c
@@ -363,6 +363,7 @@ typedef struct VideoState {
     AVFilterContext *in_audio_filter;   // the first filter in the audio chain
     AVFilterContext *out_audio_filter;  // the last filter in the audio chain
     AVFilterGraph *agraph;              // audio filter graph
+    int last_aserial;
 #endif
 #endif
 
@@ -2301,27 +2302,20 @@ static int audio_thread(void *arg)
 {
     VideoState *is = arg;
     AVFrame *frame = av_frame_alloc();
-    Frame *af;
-#if CONFIG_AVFILTER
-    int last_serial = -1;
-    int64_t dec_channel_layout;
-    int reconfigure;
-#endif
-    int got_frame = 0;
-    AVRational tb;
     int ret = 0;
 
     if (!frame)
         return AVERROR(ENOMEM);
 
     do {
+        int got_frame;
         if ((got_frame = decoder_decode_frame(&is->auddec, frame, NULL)) < 0)
             goto the_end;
 
         if (got_frame) {
-                tb = (AVRational){1, frame->sample_rate};
-
 #if CONFIG_AVFILTER
+                int reconfigure;
+                int64_t dec_channel_layout;
                 dec_channel_layout = get_valid_channel_layout(frame->channel_layout, av_frame_get_channels(frame));
 
                 reconfigure =
@@ -2329,7 +2323,7 @@ static int audio_thread(void *arg)
                                    frame->format, av_frame_get_channels(frame))    ||
                     is->audio_filter_src.channel_layout != dec_channel_layout ||
                     is->audio_filter_src.freq           != frame->sample_rate ||
-                    is->auddec.pkt_serial               != last_serial;
+                    is->auddec.pkt_serial               != is->last_aserial;
 
                 if (reconfigure) {
                     char buf1[1024], buf2[1024];
@@ -2337,14 +2331,14 @@ static int audio_thread(void *arg)
                     av_get_channel_layout_string(buf2, sizeof(buf2), -1, dec_channel_layout);
                     av_log(NULL, AV_LOG_DEBUG,
                            "Audio frame changed from rate:%d ch:%d fmt:%s layout:%s serial:%d to rate:%d ch:%d fmt:%s layout:%s serial:%d\n",
-                           is->audio_filter_src.freq, is->audio_filter_src.channels, av_get_sample_fmt_name(is->audio_filter_src.fmt), buf1, last_serial,
+                           is->audio_filter_src.freq, is->audio_filter_src.channels, av_get_sample_fmt_name(is->audio_filter_src.fmt), buf1, is->last_aserial,
                            frame->sample_rate, av_frame_get_channels(frame), av_get_sample_fmt_name(frame->format), buf2, is->auddec.pkt_serial);
 
                     is->audio_filter_src.fmt            = frame->format;
                     is->audio_filter_src.channels       = av_frame_get_channels(frame);
                     is->audio_filter_src.channel_layout = dec_channel_layout;
                     is->audio_filter_src.freq           = frame->sample_rate;
-                    last_serial                         = is->auddec.pkt_serial;
+                    is->last_aserial                    = is->auddec.pkt_serial;
 
                     if ((ret = configure_audio_filters(is, afilters, 1)) < 0)
                         goto the_end;
@@ -2354,8 +2348,11 @@ static int audio_thread(void *arg)
                 goto the_end;
 
             while ((ret = av_buffersink_get_frame_flags(is->out_audio_filter, frame, 0)) >= 0) {
-                tb = is->out_audio_filter->inputs[0]->time_base;
+                AVRational tb = is->out_audio_filter->inputs[0]->time_base;
+#else
+                AVRational tb = (AVRational){1, frame->sample_rate};
 #endif
+                Frame *af;
                 if (!(af = frame_queue_peek_writable(&is->sampq)))
                     goto the_end;
 
@@ -2994,6 +2991,9 @@ static int stream_component_open(VideoState *is, int stream_index)
             is->auddec.start_pts = is->audio_st->start_time;
             is->auddec.start_pts_tb = is->audio_st->time_base;
         }
+#if CONFIG_AVFILTER
+        is->last_aserial = -1;
+#endif
         if ((ret = decoder_start(&is->auddec, audio_thread, is)) < 0)
             goto fail;
         SDL_PauseAudio(0);
